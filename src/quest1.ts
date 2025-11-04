777d7b1f7b50f6aabc0370f6db1b5e852a5933b4
Lucas Franco <l.franco@smart-reporting.com>
2025-11-04 23:57:41 +0100

    Quest 1, part 2

--- src/quest1.ts
+++ src/quest1.ts
@@ -1,14 +1,18 @@
 import { parseLines, runQuest } from './utils.js';
 
-/**
- * Quest 1: [Problem Title]
- */
-
-function solvePart1(input: string): string {
+function getNamesAndInstructions(input: string): {
+    names: string[];
+    instructions: string[];
+} {
     const lines = parseLines(input);
-
     const names = lines[0]?.split(',') || [];
     const instructions = lines[2]?.split(',') || [];
+    return { names, instructions };
+}
+
+function solvePart1(input: string): string {
+    const { names, instructions } = getNamesAndInstructions(input);
+    const namesCount = names.length;
     let nameIndex = 0;
 
     for (const instruction of instructions) {
@@ -18,16 +22,39 @@ function solvePart1(input: string): string {
         nameIndex =
             direction === 'L'
                 ? Math.max(nameIndex - steps, 0)
-                : Math.min(nameIndex + steps, names.length - 1);
+                : Math.min(nameIndex + steps, namesCount - 1);
     }
 
     return names[nameIndex] || '';
 }
 
-function solvePart2(input: string): number {
-    const lines = parseLines(input);
-    // TODO: Implement your solution for part 2
-    return lines.length;
+function solvePart2(input: string): string {
+    const { names, instructions } = getNamesAndInstructions(input);
+    const namesCount = names.length;
+    let nameIndex = 0;
+
+    for (const instruction of instructions) {
+        const direction = instruction[0];
+        const steps = parseInt(instruction.slice(1), 10);
+
+        if (direction === 'L') {
+            nameIndex = nameIndex - steps;
+            if (nameIndex < 0) {
+                if (Math.abs(nameIndex) > namesCount) {
+                    nameIndex = namesCount - (Math.abs(nameIndex) % namesCount);
+                } else {
+                    nameIndex = namesCount - Math.abs(nameIndex);
+                }
+            }
+        } else {
+            nameIndex = nameIndex + steps;
+            if (nameIndex >= namesCount) {
+                nameIndex = nameIndex % namesCount;
+            }
+        }
+    }
+
+    return names[nameIndex] || '';
 }
 
 function solvePart3(input: string): number {
